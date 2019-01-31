ce1a77abe57033a410da02e652060c9e56e49f23
zhangxiaode <2279323443@qq.com>
2019-01-31 17:36:37 +0800

    首页微调

--- src/view/index/index.jsx
+++ src/view/index/index.jsx
@@ -4,10 +4,32 @@ import { connect } from 'react-redux'
 import {mapStateToProps, mapDispatchToProps} from '../../store/mapToProps'
 import './index.less'
 
+import ajax from '../../utils/ajax'
+
 class Index extends Component {
   state = {
   }
   componentDidMount() {
+    ajax.get('/findUser', {}).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+    ajax.get('/addUser', {}).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+    ajax.get('/updateUser', {}).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+    ajax.get('/deleteUser', {}).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   }
   switchPage(index) {
   }
